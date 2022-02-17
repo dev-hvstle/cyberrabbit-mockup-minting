c352018a38bc88e180c55a315fa5198d5610be47
rezarekt <theveyst12@gmail.com>
2022-02-18 04:38:16 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,10 +13,20 @@ const Minting = () => {
             setUserAddress(tempUserAddress);
         }
     }
+    
   return (
     <>
         <p>User Address: {userAddress}</p>
         <button onClick={connectWallet}>Connect Wallet</button>
+        <p>Number of Owned NFTs: </p>
+        <p>IDs of Owned NFTs: </p>
+        <p>Dutch Sale (Current) Price: </p>
+        <p>Whitelist Sale Price: </p>
+        <input type='text' placeholder='Number of mint'></input>
+        <button>Dutch Sale Mint</button>
+        <br></br><br></br>
+        <input type='text' placeholder='Number of mint'></input>
+        <button>Whitelist Sale Mint</button>
     </>
   )
 }
